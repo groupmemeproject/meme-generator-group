f5b333faadb0ba878eea54496c550ca91a2f9861
Jonathan Swift <jonathan.swift9k@gmail.com>
2021-06-09 13:05:57 -0600

    edit button functionality on SavedMeme working, needs refresh button and styling.

--- meme-list/src/App.js
+++ meme-list/src/App.js
@@ -13,6 +13,7 @@ class App extends React.Component{
         }
         this.saveMeme = this.saveMeme.bind(this)
         this.deleteMeme = this.deleteMeme.bind(this)
+        this.editMeme = this.editMeme.bind(this)
     }
 
     componentDidMount() {
@@ -32,14 +33,14 @@ class App extends React.Component{
         //Function passed to Save button inside Meme Component, pushes Meme State into savedMemes arr
             this.setState(prevState => {
                 return {
-                savedMemes: [...prevState.savedMemes, newMeme]
+                savedMemes: [...prevState.savedMemes, newMeme],
                 }   
             })
     }
 
     deleteMeme(id) {
         //Deletes saved meme by Id
-        const deletedMeme = this.state.savedMemes.filter(item => item.id !== id);
+        const deletedMeme = this.state.savedMemes.filter(meme => meme.id !== id);
         // console.log(deletedMeme)
         this.setState(prevState => {
             return {
@@ -48,10 +49,30 @@ class App extends React.Component{
         })
     }
 
+    editMeme(id, editedMeme) {
+        const edits = this.state.savedMemes.map(meme => {
+            if(meme.id === id) {
+                meme.bottomText = editedMeme.bottomText
+                meme.topText = editedMeme.topText
+                return meme
+            } 
+            return meme
+        })
+
+        // console.log(edits)
+            
+        this.setState(prevState => {
+            return {
+                savedMemes: prevState.savedMemes = [...edits]
+            }
+        })
+    }
+
+        
 
     render() {
-        const memesArr = this.state.memes.map(meme => <Meme key={meme.id} id={meme.id} name={meme.name} img={meme.url} save={this.saveMeme}/>)
-        const savedMemes = this.state.savedMemes.map(meme => <SavedMeme key={meme.id} id={meme.id} name={meme.name} img={meme.imgUrl} topText={meme.topText} bottomText={meme.bottomText} delete={this.deleteMeme} />)
+        const memesArr = this.state.memes.map(meme => <Meme key={Number(Math.floor(Math.random() * 1000000))} id={meme.id} name={meme.name} img={meme.url} save={this.saveMeme}/>)
+        const savedMemes = this.state.savedMemes.map(meme => <SavedMeme key={Number(Math.floor(Math.random() * 1000000))} id={meme.id} name={meme.name} img={meme.imgUrl} topText={meme.topText} bottomText={meme.bottomText} delete={this.deleteMeme} edit={this.editMeme} />)
         const randomMeme = memesArr[Math.floor(Math.random() * memesArr.length)]
         // console.log(this.state.savedMemes);
         
